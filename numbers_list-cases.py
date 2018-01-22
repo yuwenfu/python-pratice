b5fb843e43977e3e634afa48fcbad923bb75ba17
YUWENFU <872681F>
2018-01-22 14:52:54 +0800

    Initial commit

---
+++ numbers_list-cases.py
@@ -0,0 +1,52 @@
+# ~ for value in range(1,5):
+    # ~ print(value)
+# ~ print('\n')
+# ~ numbers = list(range(1,6))
+# ~ print(numbers)
+# ~ even_numbers = list(range(2,11,2))
+# ~ print(even_numbers)
+# ~ print('\n')
+# ~ squares = []
+# ~ for value in range(1,11):
+    # ~ square=value**2
+    # ~ squares.append(square)
+# ~ print(squares)
+# ~ print('\n')
+
+# ~ digits = [1,2,3,4,5,6,7,8,9,0]
+# ~ num = min(digits)
+# ~ print(num)
+# ~ num = max(digits)
+# ~ print(num)
+# ~ num = sum(digits)
+# ~ print(num)
+# ~ print('\n')
+
+# ~ squares = [value**2 for value in range(1,11)]
+# ~ print(squares)
+
+#P54 4-3
+# ~ for value in range(1,21):
+    # ~ print(value)
+#P54 4-4
+# ~ tables = [value for value in range(1,1000000)]
+# ~ print(tables)
+#P54 4-5
+# ~ tables = [value for value in range(1,1000001)]
+# ~ print(sum(tables))
+#P54 4-6
+# ~ odd_numbers = list(range(1,20,2))
+# ~ print(odd_numbers)
+#P54 4-7
+# ~ times3_nums = list(range(3,31,3))
+# ~ print(times3_nums)
+# ~ for num in times3_nums:
+    # ~ print(num)
+#P54 4-8
+cubes = [value**3 for value in range(1,11)]
+for cube in cubes:
+    print(cube)
+#P54 4-9
+cubes = [value**3 for value in range(1,11)]
+for cube in cubes:
+    print(cube)
